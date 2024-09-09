01578263a00fb162f415ff97d77efde8ddf94c65
markbumpy <markbumpy91@gmail.com>
2024-09-09 11:58:53 +0100

    fixing cors error

--- src/dela/settings.py
+++ src/dela/settings.py
@@ -48,9 +48,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -59,12 +59,20 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "https://deala-frontend-mark-kaaves-projects-7185adf0.vercel.app",
-    "https://dealapp.vercel.app",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://localhost:3000",
+#     "https://deala-frontend-mark-kaaves-projects-7185adf0.vercel.app",
+#     "https://dealapp.vercel.app",
+# ]
+
+
+
+CORS_ALLOW_ALL_ORIGINS = True  # Temporary for debugging
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_METHODS = ['*']
+
 
 
 
